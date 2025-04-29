3cbb3e0567bee7f5adb2706a09747239844367bb
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-04-29 17:13:39 -0600

    test

--- src/components/test/test8.js
+++ src/components/test/test8.js
@@ -10,13 +10,13 @@ class Test8 extends Component {
   render() {
     return (
       <div>
-        <h4>Test 8 mobile version 4</h4>
+        <h4>Test 8</h4>
         <p>Class component</p>
         <p>Exports Default</p>
         <p>A state 'name' was added in a Class Component and changes with the button: {this.state.name}</p>
       </div>
     );
-  }
+ ñ }
 }
 
 export default Test8;
