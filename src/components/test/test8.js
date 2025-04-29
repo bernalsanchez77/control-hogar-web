be164f962ab2b42c25daad95b206ae6c4aa573a3
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-04-29 17:32:41 -0600

    ff

--- src/components/test/test8.js
+++ src/components/test/test8.js
@@ -10,7 +10,7 @@ class Test8 extends Component {
   render() {
     return (
       <div>
-        <h4>Test 8</h4>
+        <h4>Test</h4>
         <p>Class component</p>
         <p>Exports Default</p>
         <p>A state 'name' was added in a Class Component and changes with the button: {this.state.name}</p>
