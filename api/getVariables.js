3e37a87bf990478055ee2be0a0b19c973f8a2d66
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-01 14:46:04 -0600

    sdf

--- api/getVariables.js
+++ api/getVariables.js
@@ -5,8 +5,14 @@ export default async function handler(req, res) {
   
     try {
       const response = await fetch('https://ifttt.massmedia.stream/api/v1/bersanesp/data');
-      const data = await response.text();
-      res.status(200).text(data); // Devuelves la respuesta JSON al cliente
+
+      const raw = await response.text();
+      const cleaned = raw.trim().replace(/^[^\[{]+/, ''); // elimina basura al inicio
+  
+      // Intentar parsear
+      const data = JSON.parse(cleaned);
+
+      res.status(200).json(data); // Devuelves la respuesta JSON al cliente
     } catch (err) {
       res.status(500).json({ error: 'Error al obtener datos', details: err.message });
     }
