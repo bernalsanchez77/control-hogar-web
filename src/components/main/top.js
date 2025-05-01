9a47ae4f999f4a336c222deb5b249bcb12afca71
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-01 14:38:48 -0600

    test

--- src/components/main/top.js
+++ src/components/main/top.js
@@ -5,7 +5,7 @@ class Top extends Component {
     super();
     this.state = {
       resetLabel: 'Reset',
-      rokuLabel: 'Cable4',
+      rokuLabel: 'Cable5',
       deviceStates: '{"LamparaComedor":"Off","LamparaSala":"Off","LamparaTurca":"Off","LamparaLava":"Off","LamparaRotatoria":"Off","Chimenea":"Off","LamparasAbajo":"Off","ParlantesSala":"Off","CalentadorNegro":"Off","CalentadorBlanco":"Off","ProyectorMute":"Off","SalaMute":"Off","CuartoMute":"off","Hdmi":"roku"}'
     };
     this.init();
