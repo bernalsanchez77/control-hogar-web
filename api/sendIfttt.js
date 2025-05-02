3420466fa749eff88bfa4b91134eb35b0e742f39
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-02 13:44:48 -0600

    zfdx

--- api/sendIfttt.js
+++ api/sendIfttt.js
@@ -3,7 +3,8 @@ export default async function handler(req, res) {
     try {
       const deviceUpperCase = device[0].toUpperCase() + device.slice(1);
       const stateUpperCase = state[0].toUpperCase() + state.slice(1);
-      const response = await fetch('https://maker.ifttt.com/trigger/' + deviceUpperCase + stateUpperCase + '/with/key/i4M0yNSEdCF7dQdEMs5e_XhA1BnQypmCTWIrlPVidUG?value1=');
+      const key = process.env.REACT_APP_SEND_KEY;
+      const response = await fetch('https://maker.ifttt.com/trigger/' + deviceUpperCase + stateUpperCase + '/with/key/' + key + '?value1=');
       res.status(200).json(response);
     } catch (err) {
       res.status(500).json({ error: 'Error al obtener datos', details: err.message });
