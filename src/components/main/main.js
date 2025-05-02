b621c4744fc24dbb1eaa82b2d8c131ff5d12cfd9
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-02 09:10:18 -0600

    df

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -40,10 +40,10 @@ class Main extends Component {
   }
   async changeDevice(device, state) {
     // fetch('/api/sendIfttt?device=' + device + '&state=' + state);
-    // this.setState(prev => ({deviceStates: {...prev.deviceStates, [device.state]: state}}), () => {
-    //   const deviceStates = this.state.deviceStates;
-    //   fetch('/api/setDeviceStates', {method: 'PUT',headers: {'Content-Type': 'application/json',},body: JSON.stringify(deviceStates)}).then(res => res.json()).then(data => {}).catch(err => {});
-    // });
+    this.setState(prev => ({deviceStates: {...prev.deviceStates, [device.state]: state}}), () => {
+      const deviceStates = this.state.deviceStates;
+      fetch('/api/setDeviceStates', {method: 'PUT',headers: {'Content-Type': 'application/json',},body: JSON.stringify(deviceStates)}).then(res => res.json()).then(data => {}).catch(err => {});
+    });
   }
   async triggerDevice(device) {
     if (this.state.deviceStates[device].state === 'On') {
@@ -56,10 +56,10 @@ class Main extends Component {
     return (
       <div>
         <div>
-        <button onClick={() => this.triggerDevice(this.state.deviceStates.LamparaComedor.id)}>{this.state.deviceStates.LamparaComedor.label}: {this.state.deviceStates.LamparaComedor.state}</button>
+        <button onClick={() => this.triggerDevice(this.state.deviceStates.LamparaComedor.id)}>{this.state.deviceStates.LamparaComedor.label}</button>
         </div>
         <div>
-        <button onClick={() => this.triggerDevice(this.state.deviceStates.LamparaTurca.id)}>{this.state.deviceStates.LamparaTurca.label}: {this.state.deviceStates.LamparaTurca.state}</button>
+        <button onClick={() => this.triggerDevice(this.state.deviceStates.LamparaTurca.id)}>{this.state.deviceStates.LamparaTurca.label}</button>
         </div>
       </div>
     );
