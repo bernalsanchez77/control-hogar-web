4abda4eccbfcb95439e6afdc9d8376597be949fd
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-02 14:51:38 -0600

    dfg

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -5,22 +5,7 @@ import { devices } from '../../global/devices';
 class Main extends Component {
   constructor() {
     super();
-    const devices = {
-      lamparaComedor: {state: 'off', label: 'Lampara Comedor', id: 'lamparaComedor'},
-      lamparaSala: {state: 'off', label: 'Lampara Sala', id: 'lamparaSala'},
-      lamparaTurca: {state: 'off', label: 'Lampara Turca', id: 'lamparaTurca'},
-      lamparaLava: {state: 'off', label: 'Lampara Lava', id: 'lamparaLava'},
-      lamparaRotatoria: {state: 'off', label: 'Lampara Rotatoria', id: 'lamparaRotatoria'},
-      chimenea: {state: 'off', label: 'Chimenea', id: 'chimenea'},
-      lamparasAbajo: {state: 'off', label: 'Lamparas Abajo', id: 'lamparasAbajo'},
-      parlantesSala: {state: 'off', label: 'Parlantes Sala', id: 'parlantesSala'},
-      palentadorNegro: {state: 'off', label: 'Calentador Lizzie', id: 'calentadorNegro'},
-      calentadorBlanco: {state: 'off', label: 'Calentador Amy', id: 'calentadorBlanco'},
-      proyectorMute: {state: 'off', label: 'Proyector Mute', id: 'proyectorMute'},
-      salaMute: {state: 'off', label: 'Sala Mute', id: 'salaMute'},
-      cuartoMute: {state: 'off', label: 'Cuarto Mute', id: 'cuartoMute'},
-      hdmi: {state: 'roku', label: 'Roku', id: 'hdmi'}
-    };
+    const devices = {};
     this.state = {devices: devices, devicesOriginal: devices};
     this.init();
   }
@@ -60,6 +45,7 @@ class Main extends Component {
   render() {
     return (
       <div>
+        <Screen></Screen>
         <div>
         <button onClick={() => this.triggerDevice(this.state.devices.lamparaComedor.id)}>{this.state.devices.lamparaComedor.label} {this.state.devices.lamparaComedor.state}</button>
         </div>
