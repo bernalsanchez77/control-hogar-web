6c6b78b5112b1566a11a82df0a95a26b40a9c825
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-02 09:49:10 -0600

    df

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -43,7 +43,7 @@ class Main extends Component {
   }
   async changeDevice(device, state) {
     // fetch('/api/sendIfttt?device=' + device + '&state=' + state);
-    this.setState(prev => ({deviceStates: {...prev.deviceStates, [device]: this.getUpdateDevice(device, state)  }}), () => {
+    this.setState(prev => ({deviceStates: {...prev.deviceStates, [device]: this.getUpdatedDevice(device, state)  }}), () => {
       const deviceStates = this.state.deviceStates;
       fetch('/api/setDeviceStates', {method: 'PUT',headers: {'Content-Type': 'application/json',},body: JSON.stringify(deviceStates)}).then(res => res.json()).then(data => {}).catch(err => {});
     });
