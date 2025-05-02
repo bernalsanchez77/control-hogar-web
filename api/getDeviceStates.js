d3d054c6ee0a959e16539846ec7fd1639424469f
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-02 10:23:10 -0600

    ere

--- api/getDeviceStates.js
+++ api/getDeviceStates.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
       return res.status(405).json({ error: 'Method Error' });
     }
     try {
-      const response = await fetch('https://ifttt.massmedia.stream/api/v1/bersanesp/data');
+      const response = await fetch('https://ifttt.massmedia.stream/api/v1/bersanesp/data2');
       const raw = await response.text();
       const cleaned = raw.trim().replace(/^[^\[{]+/, '');
       const data = JSON.parse(cleaned);
