2d7e51e859db2e3c43a7e15ec09a74986448bc27
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-02 09:44:55 -0600

    dfdf

--- api/sendIfttt.js
+++ api/sendIfttt.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
-    const { device, status } = req.query;
+    const { device, state } = req.query;
     try {
-      const response = await fetch('https://maker.ifttt.com/trigger/' + device + status + '/with/key/i4M0yNSEdCF7dQdEMs5e_XhA1BnQypmCTWIrlPVidUG?value1=');
+      const response = await fetch('https://maker.ifttt.com/trigger/' + device + state + '/with/key/i4M0yNSEdCF7dQdEMs5e_XhA1BnQypmCTWIrlPVidUG?value1=');
       res.status(200).json(response);
     } catch (err) {
       res.status(500).json({ error: 'Error al obtener datos', details: err.message });
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -4,52 +4,52 @@ class Main extends Component {
   constructor() {
     super();
     const states = {
-      LamparaComedor: {status: 'Off', label: 'Lampara Comedor', id: 'LamparaComedor'},
-      LamparaSala: {status: 'Off', label: 'Lampara Sala', id: 'LamparaSala'},
-      LamparaTurca: {status: 'Off', label: 'Lampara Turca', id: 'LamparaTurca'},
-      LamparaLava: {status: 'Off', label: 'Lampara Lava', id: 'LamparaLava'},
-      LamparaRotatoria: {status: 'Off', label: 'Lampara Rotatoria', id: 'LamparaRotatoria'},
-      Chimenea: {status: 'Off', label: 'Chimenea', id: 'Chimenea'},
-      LamparasAbajo: {status: 'Off', label: 'Lamparas Abajo', id: 'LamparasAbajo'},
-      ParlantesSala: {status: 'Off', label: 'Parlantes Sala', id: 'ParlantesSala'},
-      CalentadorNegro: {status: 'Off', label: 'Calentador Lizzie', id: 'CalentadorNegro'},
-      CalentadorBlanco: {status: 'Off', label: 'Calentador Amy', id: 'CalentadorBlanco'},
-      ProyectorMute: {status: 'Off', label: 'Proyector Mute', id: 'ProyectorMute'},
-      SalaMute: {status: 'Off', label: 'Sala Mute', id: 'SalaMute'},
-      CuartoMute: {status: 'Off', label: 'Cuarto Mute', id: 'CuartoMute'},
-      Hdmi: {status: 'roku', label: 'Hdmi', id: 'Hdmi'},
+      LamparaComedor: {state: 'Off', label: 'Lampara Comedor', id: 'LamparaComedor'},
+      LamparaSala: {state: 'Off', label: 'Lampara Sala', id: 'LamparaSala'},
+      LamparaTurca: {state: 'Off', label: 'Lampara Turca', id: 'LamparaTurca'},
+      LamparaLava: {state: 'Off', label: 'Lampara Lava', id: 'LamparaLava'},
+      LamparaRotatoria: {state: 'Off', label: 'Lampara Rotatoria', id: 'LamparaRotatoria'},
+      Chimenea: {state: 'Off', label: 'Chimenea', id: 'Chimenea'},
+      LamparasAbajo: {state: 'Off', label: 'Lamparas Abajo', id: 'LamparasAbajo'},
+      ParlantesSala: {state: 'Off', label: 'Parlantes Sala', id: 'ParlantesSala'},
+      CalentadorNegro: {state: 'Off', label: 'Calentador Lizzie', id: 'CalentadorNegro'},
+      CalentadorBlanco: {state: 'Off', label: 'Calentador Amy', id: 'CalentadorBlanco'},
+      ProyectorMute: {state: 'Off', label: 'Proyector Mute', id: 'ProyectorMute'},
+      SalaMute: {state: 'Off', label: 'Sala Mute', id: 'SalaMute'},
+      CuartoMute: {state: 'Off', label: 'Cuarto Mute', id: 'CuartoMute'},
+      Hdmi: {state: 'roku', label: 'Hdmi', id: 'Hdmi'},
     };
-    this.state = {states: states, deviceStates: states};
+    this.state = {states: states,deviceStates: states};
     this.init();
   }
   async init() {
-    this.getStatus();
-    setInterval(() => {this.getStatus();}, 5000);
+    this.getStates();
+    setInterval(() => {this.getStates();}, 5000);
   }
-  async getStatus() {
-    fetch('/api/getDeviceStatus').then(res => res.json()).then(
-      deviceStates => {this.setState({deviceStates: this.getUpdatedDeviceStatus(deviceStates)})}
+  async getStates() {
+    fetch('/api/getDeviceStates').then(res => res.json()).then(
+      deviceStates => {this.setState({deviceStates: this.getUpdatedDeviceStates(deviceStates)})}
     ).catch(err => {});
   }
-  getUpdatedDeviceStatus(deviceStates) {
+  getUpdatedDeviceStates(deviceStates) {
     const updatedDeviceStates = {};
     for (let device in this.state.deviceStates) {
-      updatedDeviceStates[device] = {...this.state.deviceStates[device], status: deviceStates[device]};
+      updatedDeviceStates[device] = {...this.state.deviceStates[device], state: deviceStates[device]};
     }
     return updatedDeviceStates;
   }
-  getUpdatedDevice(device, status) {
-    return {...this.state.deviceStates[device], status: status};
+  getUpdatedDevice(device, state) {
+    return {...this.state.deviceStates[device], state: state};
   }
-  async changeDevice(device, status) {
-    // fetch('/api/sendIfttt?device=' + device + '&status=' + status);
-    this.setState(prev => ({deviceStates: {...prev.deviceStates, [device]: this.getUpdatedDevice(device, status)  }}), () => {
+  async changeDevice(device, state) {
+    // fetch('/api/sendIfttt?device=' + device + '&state=' + state);
+    this.setState(prev => ({deviceStates: {...prev.deviceStates, [device]: this.getUpdateDevice(device, state)  }}), () => {
       const deviceStates = this.state.deviceStates;
-      fetch('/api/setDeviceStatus', {method: 'PUT',headers: {'Content-Type': 'application/json',},body: JSON.stringify(deviceStates)}).then(res => res.json()).then(data => {}).catch(err => {});
+      fetch('/api/setDeviceStates', {method: 'PUT',headers: {'Content-Type': 'application/json',},body: JSON.stringify(deviceStates)}).then(res => res.json()).then(data => {}).catch(err => {});
     });
   }
   async triggerDevice(device) {
-    if (this.state.deviceStates[device].status === 'On') {
+    if (this.state.deviceStates[device].state === 'On') {
       this.changeDevice(device, 'Off');
     } else {
       this.changeDevice(device, 'On');
@@ -59,10 +59,10 @@ class Main extends Component {
     return (
       <div>
         <div>
-        <button onClick={() => this.triggerDevice(this.state.deviceStates.LamparaComedor.id)}>{this.state.deviceStates.LamparaComedor.label} {this.state.deviceStates.LamparaComedor.label}</button>
+        <button onClick={() => this.triggerDevice(this.state.deviceStates.LamparaComedor.id)}>{this.state.deviceStates.LamparaComedor.label} {this.state.deviceStates.LamparaComedor.id}</button>
         </div>
         <div>
-        <button onClick={() => this.triggerDevice(this.state.deviceStates.LamparaTurca.id)}>{this.state.deviceStates.LamparaTurca.label} {this.state.deviceStates.LamparaTurca.label}</button>
+        <button onClick={() => this.triggerDevice(this.state.deviceStates.LamparaTurca.id)}>{this.state.deviceStates.LamparaTurca.label} {this.state.deviceStates.LamparaTurca.id}</button>
         </div>
       </div>
     );
