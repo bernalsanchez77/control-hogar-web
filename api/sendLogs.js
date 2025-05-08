a7b5503436d5ea011f8219ba563625681972249b
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-08 12:04:35 -0600

    df

--- api/sendLogs.js
+++ api/sendLogs.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     if (req.method === 'POST') {
-      const { message } = JSON.parse(req.body);
-      console.log('Log desde el frontend:', message.message);
+      const { message } = req.body;
+      console.log('Log desde el frontend:', message);
       return res.status(200).json({ ok: true });
     }
     res.status(405).json({ error: 'Método no permitido' });
