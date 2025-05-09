4f319e47154b8ec9ddd5ee2a77d64390f9e780f5
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-09 13:04:28 -0600

    dffd

--- api/sendIfttt.js
+++ api/sendIfttt.js
@@ -1,8 +1,6 @@
 export default async function handler(req, res) {
     const {device, key, value} = req.query;
     try {
-      const deviceUpperCase = device[0].toUpperCase() + device.slice(1);
-      const stateUpperCase = state[0].toUpperCase() + state.slice(1);
       const token = process.env.REACT_APP_SEND_KEY;
       let response = '';
       console.log('ifttt: ', 'https://maker.ifttt.com/trigger/' + device + '/with/key/' + token + '?value1=' + key + '&value2=' + value);
