c9a858897bcd028848e867008506138d75cdedbb
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-09 11:50:39 -0600

    dfdf

--- api/sendIfttt.js
+++ api/sendIfttt.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
       let response = '';
       if (nuevo) {
         console.log('ifttt: ', 'https://maker.ifttt.com/trigger/' + device + '/with/key/' + key + '?value1=' + state);
-        response = await fetch('https://maker.ifttt.com/trigger/' + device + '/with/key/' + key + '?value1=' + state);
+        // response = await fetch('https://maker.ifttt.com/trigger/' + device + '/with/key/' + key + '?value1=' + state);
       } else {
         response = await fetch('https://maker.ifttt.com/trigger/' + deviceUpperCase + stateUpperCase + '/with/key/' + key);
       }
