3194201bba4cea264cf29d150e2290813892e2ac
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-13 16:19:30 -0600

    sdsd

--- src/components/main/controls/top/top.js
+++ src/components/main/controls/top/top.js
@@ -34,7 +34,7 @@ function Controls({devicesState, screenSelected, triggerControlParent}) {
   }
   const triggerInput = () => {
     if (screenSelected === devicesState.proyectorSala.id) {
-      if (devicesState[screenSelected].input === 'hdmi1') {
+      if (devicesState[screenSelected].input.state === 'hdmi1') {
         triggerControlParent([devicesState[screenSelected].id], ['input','state'], 'hdmi2');
       } else {
         triggerControlParent([devicesState[screenSelected].id], ['input','state'], 'hdmi1');
