a27af4c6ab4d4760be4b37a8dbe019cb2342d40f
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-17 22:17:59 -0600

    ddf

--- src/global/devices.js
+++ src/global/devices.js
@@ -1,17 +1,110 @@
 export const devicesOriginal = {
-    lamparaComedor: {state: 'off', label: 'Lampara Comedor', id: 'lamparaComedor'},
-    lamparaSala: {state: 'off', label: 'Lampara Sala', id: 'lamparaSala'},
-    lamparaTurca: {state: 'off', label: 'Lampara Turca', id: 'lamparaTurca'},
-    lamparaLava: {state: 'off', label: 'Lampara Lava', id: 'lamparaLava'},
-    lamparaRotatoria: {state: 'off', label: 'Lampara Rotatoria', id: 'lamparaRotatoria'},
-    chimeneaSala: {state: 'off', label: 'Chimenea Sala', id: 'chimeneaSala'},
-    lamparasAbajo: {state: 'off', label: 'Lamparas Abajo', id: 'lamparasAbajo'},
-    parlantesSala: {state: 'off', label: 'Parlantes Sala', id: 'parlantesSala'},
-    calentadorNegro: {state: 'off', label: 'Calentador Lizzie', id: 'calentadorNegro'},
-    calentadorBlanco: {state: 'off', label: 'Calentador Amy', id: 'calentadorBlanco'},
-    hdmiSala: {state: 'roku', label: {roku: 'Stream', cable: 'Cable'}, id: 'hdmiSala'},
-    teleCuarto: {state: 'off', label: 'Tele Cuarto', id: 'teleCuarto', mute: 'off', volume: 0, channelSelected: 'siete', input: {state: 'hdmi1', label: {hdmi1: 'Roku/Cable', hdmi2: 'Chromecast'}}},
-    teleSala: {state: 'off', label: 'Tele Sala', id: 'teleSala', mute: 'off', volume: 0, channelSelected: 'siete', input: {state: 'hdmi1', label: {hdmi1: 'Roku/Cable', hdmi2: 'Fire stick'}}},
-    proyectorSala: {state: 'off', label: 'Proyector Sala', id: 'proyectorSala', mute: 'off', volume: 0, input: {state: 'hdmi1', label: {hdmi1: 'Roku/Cable', hdmi2: 'Playstation'}}},
-    proyectorSwitchSala: {state: 'off', label: 'Proyector Switch Sala', id: 'proyectorSwitchSala'}
+    lamparaComedor: {
+        state: 'off',
+        label: 'Lampara Comedor',
+        id: 'lamparaComedor'
+    },
+    lamparaSala: {
+        state: 'off',
+        label: 'Lampara Sala',
+        id: 'lamparaSala'
+    },
+    lamparaTurca: {
+        state: 'off',
+        label: 'Lampara Turca',
+        id: 'lamparaTurca'
+    },
+    lamparaLava: {
+        state: 'off',
+        label: 'Lampara Lava',
+        id: 'lamparaLava'
+    },
+    lamparaRotatoria: {
+        state: 'off',
+        label: 'Lampara Rotatoria',
+        id: 'lamparaRotatoria'
+    },
+    chimeneaSala: {
+        state: 'off',
+        label: 'Chimenea Sala',
+        id: 'chimeneaSala'
+    },
+    lamparasAbajo: {
+        state: 'off',
+        label: 'Lamparas Abajo',
+        id: 'lamparasAbajo'
+    },
+    parlantesSala: {
+        state: 'off',
+        label: 'Parlantes Sala',
+        id: 'parlantesSala'
+    },
+    calentadorNegro: {
+        state: 'off',
+        label: 'Calentador Lizzie',
+        id: 'calentadorNegro'
+    },
+    calentadorBlanco: {
+        state: 'off',
+        label: 'Calentador Amy',
+        id: 'calentadorBlanco'
+    },
+    hdmiSala: {
+        state: 'roku',
+        label: {
+        roku: 'Stream',
+        cable: 'Cable'
+        },
+        id: 'hdmiSala'
+    },
+    teleCuarto: {
+        state: 'off',
+        label: 'Tele Cuarto',
+        id: 'teleCuarto',
+        mute: 'off',
+        volume: '0',
+        channelSelected: 'siete',
+        input: {
+        state: 'hdmi1',
+        label: {
+            hdmi1: 'Roku/Cable',
+            hdmi2: 'Chromecast'
+        }
+        }
+    },
+    teleSala: {
+        state: 'off',
+        label: 'Tele Sala',
+        id: 'teleSala',
+        mute: 'off',
+        volume: '0',
+        channelSelected: 'siete',
+        input: {
+        state: 'hdmi1', 
+        label: {
+            hdmi1: 'Roku/Cable',
+            hdmi2: 'Fire stick'
+        }
+        }
+    },
+    proyectorSala: {
+        state: 'off',
+        label: 'Proyector Sala',
+        id: 'proyectorSala',
+        mute: 'off',
+        volume: '0',
+        input: {
+        state: 'hdmi1',
+        label: {
+            hdmi1: 'Roku/Cable',
+            hdmi2: 'Playstation'
+        }
+        }
+    },
+    proyectorSwitchSala: {
+        state: 'off',
+        label: 'Proyector Switch Sala',
+        id: 'proyectorSwitchSala'
+    }
 };
+
