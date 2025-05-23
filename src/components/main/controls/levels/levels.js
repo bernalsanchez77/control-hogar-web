287e598edcdd63f37908a194be79300701dbf2af
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-23 14:13:30 -0600

    cfdf

--- src/components/main/controls/levels/levels.js
+++ src/components/main/controls/levels/levels.js
@@ -5,6 +5,7 @@ function Levels({ devicesState, screenSelected, credential, triggerControlParent
   const timeout3s = useRef(null);
   const timeout6s = useRef(null);
   const volumeChange = useRef('1');
+  const volumeShow = useRef(null);
   const triggerMute = () => {
     navigator.vibrate([200]);
     if (devicesState[screenSelected].mute === 'on') {
@@ -53,15 +54,26 @@ function Levels({ devicesState, screenSelected, credential, triggerControlParent
     volumeChange.current = '1';
     timeout3s.current = setTimeout(() => {
       volumeChange.current = '5';
-    }, 2000);
+      if (button === 'down') {
+        volumeShow.current = parseInt(devicesState[screenSelected].volume) - parseInt(volumeChange.current);
+      } else {
+        volumeShow.current = parseInt(devicesState[screenSelected].volume) + parseInt(volumeChange.current);
+      }
+    }, 1000);
     timeout6s.current = setTimeout(() => {
       volumeChange.current = '10';
-    }, 4000);
+      if (button === 'down') {
+        volumeShow.current = parseInt(devicesState[screenSelected].volume) - parseInt(volumeChange.current);
+      } else {
+        volumeShow.current = parseInt(devicesState[screenSelected].volume) + parseInt(volumeChange.current);
+      }
+    }, 2000);
   }
 
   const triggerVolumeEnd = (button) => {
     clearTimeout(timeout3s.current);
     clearTimeout(timeout6s.current);
+    volumeShow.current = null;
     triggerVolume(volumeChange.current, button);
   }
       
@@ -69,12 +81,23 @@ function Levels({ devicesState, screenSelected, credential, triggerControlParent
     <div className='controls-levels'>
       <div className='controls-levels-row'>
         <div className='controls-levels-element controls-levels-element--left'>
+          {credential === 'dev' && 
           <button
             onContextMenu={(e) => e.preventDefault()}
             className='controls-levels-button'
-            onClick={() => triggerVolume('1','up')}>
+            onTouchStart={() => triggerVolumeStart('up')}
+            onTouchEnd={() => triggerVolumeEnd('up')}>
             &#9650;
           </button>
+          }
+          {credential !== 'dev' && 
+          <button
+            onContextMenu={(e) => e.preventDefault()}
+            className='controls-levels-button'
+            onClick={() => triggerVolume('1', 'up')}>
+            &#9650;
+          </button>
+          }
         </div>
         {
           devicesState.hdmiSala.state === 'roku' &&
@@ -106,7 +129,7 @@ function Levels({ devicesState, screenSelected, credential, triggerControlParent
       <div className='controls-levels-row controls-levels-row--mute'>
         <div className='controls-levels-element controls-levels-element--mute'>
           <span className='controls-levels-span'>
-            vol {devicesState[screenSelected].volume}
+            vol {volumeShow.current || devicesState[screenSelected].volume}
           </span>
         </div>
         <div className='controls-levels-element controls-levels-element--mute-icon'>
