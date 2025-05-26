e7f9dda0608330187a9655e1c0cb65fe84d4ae0f
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-26 14:14:30 -0600

    dfdf

--- src/components/main/controls/apps/apps.js
+++ src/components/main/controls/apps/apps.js
@@ -15,41 +15,7 @@ function Apps({devicesState, screenSelected, triggerControlParent}) {
   return (
     <div className='controls-apps'>
       <div className='controls-apps-row'>
-        <div className='controls-apps-element'>
-          <button
-            className={`controls-apps-button ${devicesState.rokuSala.apps.netflix.id === devicesState.rokuSala.app ? "controls-apps-button--on" : "controls-apps-button--off"}`}
-            onTouchStart={() => triggerControl(devicesState.rokuSala.apps.netflix.id)}>
-              {devicesState.rokuSala.apps.netflix.label}
-          </button>
-        </div>
-        <div className='controls-apps-element'>
-          <button
-            className={`controls-apps-button ${devicesState.rokuSala.apps.disney.id === devicesState.rokuSala.app ? "controls-apps-button--on" : "controls-apps-button--off"}`}
-            onTouchStart={() => triggerControl(devicesState.rokuSala.apps.disney.id)}>
-              {devicesState.rokuSala.apps.disney.label}
-          </button>
-        </div>
-        <div className='controls-apps-element'>
-          <button
-            className={`controls-apps-button ${devicesState.rokuSala.apps.youtube.id === devicesState.rokuSala.app ? "controls-apps-button--on" : "controls-apps-button--off"}`}
-            onTouchStart={() => triggerControl(devicesState.rokuSala.apps.youtube.id)}>
-              {devicesState.rokuSala.apps.youtube.label}
-          </button>
-        </div>
-        <div className='controls-apps-element'>
-          <button
-            className={`controls-apps-button ${devicesState.rokuSala.apps.max.id === devicesState.rokuSala.app ? "controls-apps-button--on" : "controls-apps-button--off"}`}
-            onTouchStart={() => triggerControl(devicesState.rokuSala.apps.max.id)}>
-              {devicesState.rokuSala.apps.max.label}
-          </button>
-        </div>
-        <div className='controls-apps-element'>
-          <button
-            className={`controls-apps-button ${devicesState.rokuSala.apps.amazon.id === devicesState.rokuSala.app ? "controls-apps-button--on" : "controls-apps-button--off"}`}
-            onTouchStart={() => triggerControl(devicesState.rokuSala.apps.amazon.id)}>
-              {devicesState.rokuSala.apps.amazon.label}
-          </button>
-        </div>
+
       </div>
     </div>
   )
