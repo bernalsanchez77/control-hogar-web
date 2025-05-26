5928d1b3b14de0769350f23a9dc9fdb262ad98b5
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-05-25 22:25:56 -0600

    Update apps.js

--- src/components/main/controls/apps/apps.js
+++ src/components/main/controls/apps/apps.js
@@ -1,1 +1,28 @@
 
+
+import React from 'react';
+import './apps.css';
+
+function Apps({devicesState, screenSelected, triggerControlParent}) {
+  const triggerControl = (value) => {
+    navigator.vibrate([100]);
+    const device = 'rokuSala';
+    triggerControlParent([device], ['command'], [value], false);
+  }
+
+  return (
+    <div className='controls-apps'>
+      <div className='controls-apps-row'>
+        <div className='controls-apps-element'>
+          <button
+            className="controls-apps-button"
+            onTouchStart={() => triggerControl('netflix')}>
+              Netflix
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Apps;
