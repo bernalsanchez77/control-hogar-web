dc086baa458ec5d49aa24ccedf6d61d399a1b867
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-06-07 01:39:45 +0000

    dfdf

--- src/components/main/controls/apps/apps.js
+++ src/components/main/controls/apps/apps.js
@@ -83,50 +83,6 @@ function Apps({devicesState, screenSelected, triggerControlParent}) {
               </img>
           </button>
         </div>
-        <div className='controls-apps-element'>
-          <button
-            className={`controls-apps-button ${devicesState.rokuSala.apps.disney.id === devicesState.rokuSala.app ? "controls-apps-button--on" : "controls-apps-button--off"}`}
-            onTouchStart={() => triggerControl(devicesState.rokuSala.apps.disney.id)}>
-              <img
-                className='controls-apps-img controls-apps-img--button'
-                src={devicesState.rokuSala.apps.disney.img}
-                alt="icono">
-              </img>
-          </button>
-        </div>
-        <div className='controls-apps-element'>
-          <button
-            className={`controls-apps-button ${devicesState.rokuSala.apps.youtube.id === devicesState.rokuSala.app ? "controls-apps-button--on" : "controls-apps-button--off"}`}
-            onTouchStart={() => triggerControl(devicesState.rokuSala.apps.youtube.id)}>
-              <img
-                className='controls-apps-img controls-apps-img--button'
-                src={devicesState.rokuSala.apps.youtube.img}
-                alt="icono">
-              </img>
-          </button>
-        </div>
-        <div className='controls-apps-element'>
-          <button
-            className={`controls-apps-button ${devicesState.rokuSala.apps.max.id === devicesState.rokuSala.app ? "controls-apps-button--on" : "controls-apps-button--off"}`}
-            onTouchStart={() => triggerControl(devicesState.rokuSala.apps.max.id)}>
-              <img
-                className='controls-apps-img controls-apps-img--button'
-                src={devicesState.rokuSala.apps.max.img}
-                alt="icono">
-              </img>
-          </button>
-        </div>
-        <div className='controls-apps-element'>
-          <button
-            className={`controls-apps-button ${devicesState.rokuSala.apps.amazon.id === devicesState.rokuSala.app ? "controls-apps-button--on" : "controls-apps-button--off"}`}
-            onTouchStart={() => triggerControl(devicesState.rokuSala.apps.amazon.id)}>
-              <img
-                className='controls-apps-img controls-apps-img--button'
-                src={devicesState.rokuSala.apps.amazon.img}
-                alt="icono">
-              </img>
-          </button>
-        </div>
       </div>
     </div>
   )
