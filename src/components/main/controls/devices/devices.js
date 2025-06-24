fb53a0732a5133abc2d8c0eae6140c2161fd5df7
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-06-24 21:57:21 +0000

    sdsd

--- src/components/main/controls/devices/devices.js
+++ src/components/main/controls/devices/devices.js
@@ -3,8 +3,8 @@ import './devices.css';
 
 function Devices({devicesState, deviceState, triggerControlParent}) {
   const triggerDevice = (color) => {
-    const device = [{device: devicesState.luzCuarto.id, ifttt: devicesState.luzCuarto.id}];
-    if (deviceState.luzCuarto.state === 'off') {
+    const device = [{device: devicesState[deviceState].id, ifttt: devicesState[deviceState].id}];
+    if (devicesState[deviceState].state === 'off') {
       triggerControlParent(device, ['state'], ['on']);
     }
     setTimeout(() => {
@@ -19,13 +19,13 @@ function Devices({devicesState, deviceState, triggerControlParent}) {
         <ul className='controls-devices-ul'>
           <li className='controls-device'>
               <button
-                className={`controls-device-button controls-device-button--white ${devicesState.luzCuarto.color === 'white' ? 'controls-device-button--selected' : ''}`}
+                className={`controls-device-button controls-device-button--white ${devicesState[deviceState].color === 'white' ? 'controls-device-button--selected' : ''}`}
                 onTouchStart={() => triggerDevice('white')}>
               </button>
           </li>
           <li className='controls-device'>
               <button
-                className={`controls-device-button controls-device-button--red ${devicesState.luzCuarto.color === 'red' ? 'controls-device-button--selected' : ''}`}
+                className={`controls-device-button controls-device-button--red ${devicesState[deviceState].color === 'red' ? 'controls-device-button--selected' : ''}`}
                 onTouchStart={() => triggerDevice('red')}>
               </button>
           </li>
