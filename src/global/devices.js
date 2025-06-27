141bfb499c6955aed2a549d81d7df1e5b46f3e18
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-06-27 21:28:59 +0000

    dfdf

--- src/global/devices.js
+++ src/global/devices.js
@@ -2,27 +2,32 @@ export const devicesOriginal = {
     lamparaComedor: {
         state: 'off',
         label: 'Lampara Comedor',
-        id: 'lamparaComedor'
+        id: 'lamparaComedor',
+        img: '/imgs/devices/lamparacomedor.png',
     },
     lamparaSala: {
         state: 'off',
         label: 'Lampara Sala',
-        id: 'lamparaSala'
+        id: 'lamparaSala',
+        img: '/imgs/devices/lamparasala.png',
     },
     lamparaTurca: {
         state: 'off',
         label: 'Lampara Turca',
-        id: 'lamparaTurca'
+        id: 'lamparaTurca',
+        img: '/imgs/devices/lamparaturca.png',
     },
     lamparaLava: {
         state: 'off',
         label: 'Lampara Lava',
-        id: 'lamparaLava'
+        id: 'lamparaLava',
+        img: '/imgs/devices/lamparalava.png',
     },
     lamparaRotatoria: {
         state: 'off',
         label: 'Lampara Rotatoria',
-        id: 'lamparaRotatoria'
+        id: 'lamparaRotatoria',
+        img: '/imgs/devices/lampararotatoria.png',
     },
     luzCuarto: {
         state: 'off',
@@ -30,31 +35,37 @@ export const devicesOriginal = {
         id: 'luzCuarto',
         color: '',
         level: '',
+        img: '/imgs/devices/luzcuarto.png',
     },
     luzEscalera: {
         state: 'off',
         label: 'Luz Escalera',
-        id: 'luzEscalera'
+        id: 'luzEscalera',
+        img: '/imgs/devices/luzescalera.png',
     },
     chimeneaSala: {
         state: 'off',
         label: 'Chimenea Sala',
-        id: 'chimeneaSala'
+        id: 'chimeneaSala',
+        img: '/imgs/devices/chimeneasala.png',
     },
     ventiladorSala: {
         state: 'off',
         label: 'Ventilador Sala',
-        id: 'ventiladorSala'
+        id: 'ventiladorSala',
+        img: '/imgs/devices/ventiladorsala.png',
     },
     lamparasAbajo: {
         state: 'off',
         label: 'Lamparas Abajo',
-        id: 'lamparasAbajo'
+        id: 'lamparasAbajo',
+        img: '/imgs/devices/lamparasabajo.png',
     },
     parlantesSala: {
         state: 'off',
         label: 'Parlantes Sala',
-        id: 'parlantesSala'
+        id: 'parlantesSala',
+        img: '/imgs/devices/parlantessala.png',
     },
     calentadorNegro: {
         state: 'off',
