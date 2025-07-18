c564464e61b9aa99ec05e80a5266d402e6086b47
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-07-18 13:51:44 -0600

    ssd

--- api/validateCredentials.js
+++ api/validateCredentials.js
@@ -1,13 +1,13 @@
 export default function handler(req, res) {
     const {key} = req.body;
     let validUser = false;
-    let dev = false;
+    let dev = '';
     if (key === process.env.REACT_APP_CREDENTIALS_KEY) {
       validUser = true;
     }
     if (key === process.env.REACT_APP_DEV_CREDENTIALS_KEY) {
       validUser = true;
-      dev = true;
+      dev = 'dev';
     }
     res.status(200).json({ status: 200, validUser: validUser, dev});
   }
