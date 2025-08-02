fc34afd659d577c079b81be97af8435d2ad2a999
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-08-02 10:22:00 -0600

    Update getVideosFromYoutube.js

--- api/getVideosFromYoutube.js
+++ api/getVideosFromYoutube.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const apiKey = process.env.YOUTUBE_API_KEY;
   const { q } = req.query; // Get search term from URL
   try {
-    const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&q=${encodeURIComponent(q)}&key=${apiKey}`;
+    const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&q=${encodeURIComponent(q)}&key=${apiKey}`;
     const response = await fetch(youtubeUrl);
     const data = await response.json();
     if (!response.ok) {
@@ -13,3 +13,4 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: err.message });
   }
 }
+
