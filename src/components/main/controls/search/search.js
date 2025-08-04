709cf38bf3a8edec751e6705c1003be0b4c1e2fe
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-08-03 19:02:51 -0600

    Update search.js

--- src/components/main/controls/search/search.js
+++ src/components/main/controls/search/search.js
@@ -13,6 +13,33 @@ function Search({devicesState, deviceState, youtubeSearchVideos, youtubeLizVideo
   return (
     <div className='controls-search'>
       <div className='controls-search-row'>
+
+
+   <form
+  className="controls-search-input-wrapper"
+  onSubmit={(e) => {
+    e.preventDefault(); // Prevent page reload
+    searchYoutube();    // Trigger the search
+  }}
+>
+  <input
+    className="controls-search-input"
+    type="search"
+    inputMode="search"
+    enterKeyHint="search"
+    placeholder="Buscar Videos"
+    value={youtubeSearchText}
+    onChange={(e) => setYoutubeSearchText(e.target.value)}
+  />
+</form>
+
+<div className="controls-search-button-wrapper">
+  <button className="controls-search-button" onClick={searchYoutube}>
+    Buscar
+  </button>
+</div>
+
+      /*
         <div className='controls-search-input-wrapper'>
           <input className='controls-search-input'
             type="text"
@@ -27,6 +54,7 @@ function Search({devicesState, deviceState, youtubeSearchVideos, youtubeLizVideo
             Buscar
           </button>
         </div>
+        */
       </div>
     </div>
   )
