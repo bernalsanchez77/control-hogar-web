35b7366a435b34745a403176fc0071df00c1b4fb
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-08-08 11:25:40 -0600

    sdsd

--- api/getYoutubeVideosFromSupabase.js
+++ api/getYoutubeVideosFromSupabase.js
@@ -11,4 +11,9 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: error.message })
   }
   res.status(200).json(data)
+
+  supabase.channel('youtube-videos-liz-changes').on(
+    'postgres_changes',
+    { event: '*', schema: 'public', table: 'youtube-videos-liz'},
+    payload => {console.log('Change received!', payload);}).subscribe();
 }
