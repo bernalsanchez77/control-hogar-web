b2e58bf9306ce375b29acd0dd3ba074ec310bf22
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-08-08 20:14:55 -0600

    dfdf

--- api/updateVideoInSupabase.js
+++ api/updateVideoInSupabase.js
@@ -6,15 +6,26 @@ const supabase = createClient(
 )
 
 export default async function handler(req, res) {
-  const {id, date} = req.body;
+  const { id, date } = req.body;
+
+  let data, error;
+
   if (date) {
-    const {data, error} = await supabase.from('youtube-videos-liz').update({date, state: 'selected'}).eq('id', id);
+    ({ data, error } = await supabase
+      .from('youtube-videos-liz')
+      .update({ date, state: 'selected' })
+      .eq('id', id));
   } else {
-    const {data, error} = await supabase.from('youtube-videos-liz').update({state: ''}).eq('id', id);
+    ({ data, error } = await supabase
+      .from('youtube-videos-liz')
+      .update({ state: '' })
+      .eq('id', id));
   }
 
   if (error) {
-    return res.status(500).json({ error: error.message })
+    return res.status(500).json({ error: error.message });
   }
-  res.status(200).json({ message: '✅ Date updated', data })
+
+  res.status(200).json({ message: '✅ Date updated', data });
 }
+
