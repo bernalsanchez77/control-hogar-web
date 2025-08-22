a43c9a7037772eefc851a8485d12240056ae29d2
bernalsanchez77 <bernalsanchez77@gmail.com>
2025-08-22 16:15:22 -0600

    dfdf

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -161,7 +161,7 @@ function Main() {
             });
           }
           if (el.device === 'teleSala' || el.device === 'teleCuarto' || el.device === 'teleCocina' || el.device === 'proyectorSala') {
-            const newId = screens.find(screen => screen.id === el.device).id;
+            const newId = screensRef.find(screen => screen.id === el.device).id;
             requests.updateTableInSupabase({
               new: {newId, newTable: 'screens', ['new' + el.key.charAt(0).toUpperCase() + el.key.slice(1)]: el.value, newDate: new Date().toISOString()}
             });
