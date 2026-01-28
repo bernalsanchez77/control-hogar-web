9601789b9e6f6c6b692980aa6eac9b92abfe3db6
bernalsanchez77 <bernalsanchez77@gmail.com>
2026-01-28 12:24:53 -0600

    sdsd

--- api/updateTableInSupabase.js
+++ api/updateTableInSupabase.js
@@ -3,30 +3,30 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SER
 export default async function handler(req, res) {
   // console.log('params: ', req.body);
 
-  let newId = null;
-  let newDate = null;
-  let newTable = null;
-  let newState = null;
-  let newVolume = null;
-  let newMute = null;
-  let newColor = null;
-  let newPlayState = null;
-  let newQueue = null;
+  let id = null;
+  let date = null;
+  let table = null;
+  let state = null;
+  let volume = null;
+  let mute = null;
+  let color = null;
+  let playState = null;
+  let queue = null;
 
   if (req.body) {
-    newId = req.body.id;
-    newDate = req.body.date;
-    newTable = req.body.table;
-    newState = req.body.state;
-    newVolume = req.body.volume;
-    newMute = req.body.mute;
-    newColor = req.body.color;
-    newPlayState = req.body.playState;
-    newQueue = req.body.queue;
+    id = req.body.id;
+    date = req.body.date;
+    table = req.body.table;
+    state = req.body.state;
+    volume = req.body.volume;
+    mute = req.body.mute;
+    color = req.body.color;
+    playState = req.body.playState;
+    queue = req.body.queue;
   }
 
   let data, error;
-  ({ data, error } = await supabase.from(newTable).update({ volume: newVolume, mute: newMute, color: newColor, date: newDate, state: newState, playState: newPlayState, queue: newQueue }).eq('id', newId));
+  ({ data, error } = await supabase.from(table).update({ volume, mute, color, date, state, playState, queue }).eq('id', id));
   if (error) {
     return res.status(500).json({ error: error.message });
   }
